6fc9d445e17848afc6bdda3817c319f4fb018107
dominikdysput <38400636+dominikdysput@users.noreply.github.com>
2019-04-15 15:11:19 +0200

    Update tree.c

--- tree.c
+++ tree.c
@@ -9,32 +9,39 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <linux/limits.h>
-
 #define KATALOG_COLOR "\033[94m"
 #define FILE_COLOR "\x1B[32m" 
 #define RESET "\033[0m"
 
-int flagd = 0; int flagf = 0; 
+int flagd = 0; int flagf = 0; int flagL = 0;
 static int how_much_files = 0;
 static int how_much_directory = 0;
-static int which_level = 0;
+static int help = 0;
 void make_space(int which_level)
 {
 	for (int i = 0; i < which_level; i++)
 	{
-		printf(" ");
+		printf("   ");
 	}
 }
-int listdir(const char *name, int which_level)
+void listdir(const char *name, int which_level)
 {
     DIR *dir;
     struct dirent *entry;
 	struct stat fileStat;
     if (!(dir = opendir(name)))
-        return which_level;
-	
+        return;
     while ((entry = readdir(dir)) != NULL) 
 	{
+		if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+					continue;
+		char path[PATH_MAX];				
+		snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);	
+		if(stat(path, &fileStat) == -1)
+		{
+			perror("stat()");
+			continue;
+			}		
 		if (flagd == 1)
 		{
 			if (which_level>0)
@@ -43,8 +50,6 @@ int listdir(const char *name, int which_level)
 			}
 			if (entry->d_type == DT_DIR) 
 			{
-				if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-					continue;
 				char path[PATH_MAX];				
 				snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);				
 				if(flagf == 1)
@@ -57,25 +62,23 @@ int listdir(const char *name, int which_level)
 				}
 				printf("%s", RESET);				
 				how_much_directory++;
+				if (flagL == 1)
+					help++;
 				listdir(path, which_level + 1);					
 			} 
 		}
 		else if(flagd==0)
 		{
+			if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+					continue;
 			if (which_level>0)
 			{
 				make_space(which_level);
 			}
 			if (entry->d_type == DT_DIR) 
 			{
-
-				if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
-					continue;
-				char path[PATH_MAX];				
-				snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);				
 				if(flagf == 1)
 				{
-					
 					printf("|--%s%s\n",KATALOG_COLOR, path);
 				}
 				else if(flagf==0)
@@ -83,6 +86,8 @@ int listdir(const char *name, int which_level)
 					printf("|--%s%s\n",KATALOG_COLOR, entry->d_name);
 				}
 				printf("%s", RESET);
+				if (flagL == 1)
+					help++;
 				how_much_directory++;
 				listdir(path, which_level + 1);					
 			} 
@@ -105,24 +110,21 @@ int listdir(const char *name, int which_level)
 			{
 				if(flagf==1)
 				{
-					char path[PATH_MAX];
-					snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);
 					printf("|--%s\n",path);
 				}
 				else if(flagf==0)
 				{	
 					printf("|--%s\n",entry->d_name);
-			
 				}
 				how_much_files++;				
 			}
 		}
     }
     closedir(dir);
-	return which_level;
 }
 int main(int argc,char **argv)
  {
+	int which_level = 0;
 	int ret, index;
 	char *cvalue = NULL; char *foldername=NULL;
 	opterr = 0; //no default "invalid option" info
@@ -130,6 +132,7 @@ int main(int argc,char **argv)
 		switch (ret) {
 			case 'd': flagd = 1; break;
 			case 'f': flagf = 1; break;
+			case 'L': flagL = 1; break;
 			case '?':
 				fprintf (stderr, "Unknown option `-%c'.\n",optopt);
 				return 1;
@@ -139,16 +142,11 @@ int main(int argc,char **argv)
 		{
 			foldername = argv[index]; break;
 		}
-
 	if(foldername==NULL)
 		foldername=".";
-
-
 	listdir(foldername, which_level);
-	
-
-	(flagd == 0) ? printf("\n\n%d directories, %d files\n", how_much_directory, how_much_files) : 
-	 printf("\n\n%d directories\n", how_much_directory);		
-
+	(flagL == 1) ? printf("\nMax level:%d\n", help) : printf("\n");	
+	(flagd == 0) ? printf("\n%d directories, %d files\n", how_much_directory, how_much_files) : 
+	 printf("%d directories\n", how_much_directory);	
     return 0;
 }
